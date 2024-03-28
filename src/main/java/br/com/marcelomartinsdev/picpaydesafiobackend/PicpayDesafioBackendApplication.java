ec8df602a230ff7c7c5cce333f4fff91bacd7499
Marcelo Martins <marcelomartinsdev@gmail.com>
2024-03-28 18:06:27 -0300

    Initial commit

---
+++ src/main/java/br/com/marcelomartinsdev/picpaydesafiobackend/PicpayDesafioBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.marcelomartinsdev.picpaydesafiobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicpayDesafioBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicpayDesafioBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/marcelomartinsdev/picpaydesafiobackend/PicpayDesafioBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.marcelomartinsdev.picpaydesafiobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicpayDesafioBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
